e471c1a9142a005584db15b185fedc8f189197fc
sagor ahmed <sagorahmed1400@gmail.com>
2025-04-23 05:44:26 +0800

    Initial project commit

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1369097789"
+    serviceWorkerVersion: "756815163"
   }
 });
